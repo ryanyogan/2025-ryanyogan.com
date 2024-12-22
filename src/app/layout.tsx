6390e7817aee615cd9a69199d2d3b2c3edc51682
Ryan Yogan <ryanyogan@gmail.com>
2024-12-22 02:08:17 -0600

    more space!

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
           <main className="sm:mt-16 mt-4 mb-24">
             <div className="container-wide">
               <div className="space-y-24">
-                <div className="space-y-8">
+                <div className="space-y-12">
                   <NameTitle />
                   {children}
                 </div>

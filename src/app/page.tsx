569496ad738e2d34a51dd7319e000660f04b063a
Ryan Yogan <ryanyogan@gmail.com>
2024-12-24 23:14:47 -0600

    tight tracking

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,17 +16,17 @@ export default async function Home() {
         <h2 className="text-3xl font-serif font-normal tracking-tight text-black pb-2">
           Hello I&apos;m Ryan,
         </h2>
-        <p className="text-base text-gray-700 max-w-5xl font-sans tracking-tight">
+        <p className="text-base text-gray-700 max-w-5xl font-sans tracking-tighter">
           As an Engineering Leader and Software Developer. I&apos;m passionate
           about crafting exceptional user experiences, developing rock-solid
           distributed services, and nurturing a dynamic 🤝 team development.
         </p>
-        <p className="text-base text-gray-700 max-w-5xl font-sans tracking-tight">
+        <p className="text-base text-gray-700 max-w-5xl font-sans tracking-tighter">
           My leadership style is rooted in empathy, curiosity, and cheerleading.
           I believe in the power of a growth mindset, and I strive to create an
           environemnt where everyone may learn, thrive, and have 🎉 fun.
         </p>
-        <p className="text-base text-gray-700 max-w-5xl font-sans tracking-tight">
+        <p className="text-base text-gray-700 max-w-5xl font-sans tracking-tighter">
           My engineering approach is that of a Technical Generalist, recognizing
           the unique value each technology brings to the table. My mission is to
           build and teach, leveraging my broad skill set to innovate and 💡

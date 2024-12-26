4148e060b49e4136cefb99c4f8b71d4f2ae3acd0
Ryan Yogan <ryanyogan@gmail.com>
2024-12-26 12:32:15 -0600

    about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,14 +7,10 @@ export const metadata = {
 export default function AboutPage() {
   return (
     <>
-      <section className="space-y-6">
-        <h2 className="text-2xl font-serif font-normal tracking-tight text-gray-900">
-          My Journey
+      <section className="space-y-4">
+        <h2 className="text-3xl font-serif font-normal tracking-tight text-black pb-2">
+          Hello I&apos;m Ryan,
         </h2>
-        Welp... this is awkward. I haven&apos;t written anything here yet. 🙈
-        I'm pushing to main like a crazy person this week and your here..
-        <br />
-        <br />
         <p className="text-base text-gray-700 tracking-tighter">
           As an Engineering Leader and Software Developer. I&apos;m passionate
           about crafting exceptional user experiences, developing rock-solid
@@ -31,6 +27,63 @@ export default function AboutPage() {
           build and teach, leveraging my broad skill set to innovate and 💡
           inspire.
         </p>
+
+        <p className="text-base text-gray-700 tracking-tighter">
+          I made a new site, with my own design for once lmao. I put some
+          serious thought into the spacing, fonts, and simplicity. I want to
+          write more, I cannot stand my voice on YouTube. I may ramble about
+          leadership, engineering, life, aviation... why not, my parents are a
+          majority of my traffic, hi mom 👋.
+        </p>
+
+        <h2 className="text-3xl font-serif font-normal tracking-tight text-black pb-2 pt-12">
+          Okay, here&apos;s what LLM&apos;s have to say about me...
+        </h2>
+
+        <p className="text-base text-gray-700 tracking-tighter">
+          Embarking on my tech journey with a 386 computer, I quickly became the
+          family IT department, graduating from building PCs to conjuring AOL
+          &quot;proggies&quot; in Visual Basic. My love affair with server racks
+          in chilly rooms was just the beginning, and my entry into theater!
+          Fast forward through a symphony of C#, PHP spells, and Rails-powered
+          rollercoasters, and you&apos;ll find me professing my love for all
+          things engineering.
+        </p>
+        <p className="text-base text-gray-700 tracking-tighter">
+          These days, I&apos;m crafting video game masterpieces in Rust—so
+          &quot;unique&quot; they&apos;re practically art, and hoarding
+          leadership books like they&apos;re going out of style. My toolkit? A
+          kaleidoscope of TypeScript frameworks so infamous, they dare not speak
+          their names.
+        </p>
+
+        <p className="text-base text-gray-700 tracking-tighter">
+          I&apos;ve navigated the startup scene from a cozy nook above a Chicago
+          bar to the dizzying heights of corporate skyscrapers, and I love them
+          all. Amidst a quirky relationship with Emacs and Clojure (we&apos;re
+          complicated), I find Zen in configuring Neovim more than I do in
+          housekeeping. I occasionally pine for the days of wrestling with
+          ColdFusion, Perl, and coaxing that last table and gif into
+          pixel-perfect harmony for IE6—ah, simpler times.
+        </p>
+        <p className="text-base text-gray-700 tracking-tighter">
+          My home doubles as an aircraft hangar, thanks to an obsession with
+          tinkering on aircrafts and simulators. I&apos;ve taken &quot;home
+          office&quot; to new altitudes, recreating a commercial flight
+          experience that&apos;s just one cranky passenger short of actual
+          takeoff.
+        </p>
+        <h2 className="text-3xl font-serif font-normal tracking-tight text-black pb-2 pt-12">
+          What matters most to me?
+        </h2>
+        <ul>
+          <li>👨‍👩‍👦‍👦 is my love.</li>
+          <li>🛩 is my joy.</li>
+          <li>🎸 is my jam.</li>
+          <li>🐕 & 🐈‍⬛ pain in the ass.</li>
+          <li>🚙 detailing cars is my zen.</li>
+          <li>🏒 full-time ice dad.</li>
+        </ul>
       </section>
     </>
   );

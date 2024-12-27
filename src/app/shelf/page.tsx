f5cde135f95f7b825d9e5f5b83ededea0ed406e3
Ryan Yogan <ryanyogan@gmail.com>
2024-12-26 21:31:31 -0600

    okay for tonight

--- src/app/shelf/page.tsx
+++ src/app/shelf/page.tsx
@@ -11,21 +11,65 @@ export const metadata = {
 export default async function ShelfPage() {
   const [repos, languages] = await Promise.all([getAllRepos(), getLanguages()]);
 
+  const leadershipMaterial = [
+    {
+      name: "Peopleware: Productive Projects & Teams",
+      description:
+        "A book on the human side of software development, and how to manage and lead software projects.",
+      url: "https://www.amazon.com/Peopleware-Productive-Projects-Tom-DeMarco/dp/0932633439",
+    },
+    {
+      name: "Engineering Managment for the Rest of Us",
+      description:
+        "This book isn't for the born leaders.  This book is for the rest of us.",
+      url: "https://www.engmanagement.dev/",
+    },
+    {
+      name: "Engineering Management Fundamentals 101",
+      description:
+        "Explore a career in software management with guidance from Netflix's Engineering Manager, Jem Young. Learn key motivations, challenges, and essential skills for effective leadership and meeting management, setting the stage for success in engineering management.",
+      url: "https://www.engmanagement.dev/",
+    },
+    {
+      name: "Enterprise Engineering Management 102",
+      description:
+        "Learn to build and lead high-performing technical teams with insights from Netflix's Ryan Burgess. Learn to establish OKRs and KPIs, develop team charters, refine hiring processes, and build strong partnerships. Enhance your leadership skills!",
+      url: "https://frontendmasters.com/courses/engineering-management/",
+    },
+  ];
+
   return (
     <>
-      <h2 className="text-2xl font-serif font-normal tracking-tight text-gray-900">
-        Nerdy Stuff First
-      </h2>
-      <header>
-        <p className="text-base text-gray-700 leading-relaxed max-w-2xl font-sans">
-          Dive into my GitHub projects and discover the technologies I work with
-          most frequently.
-        </p>
-      </header>
+      <section className="space-y-4">
+        <h2 className="sm:text-3xl text-2xl font-serif font-normal tracking-tight text-black pb-2">
+          Books First.
+        </h2>
+        <ul className="mt-4 space-y-2">
+          {leadershipMaterial.map((language) => (
+            <li key={language.name}>
+              <article className="group pb-4">
+                <a
+                  href={language.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="flex flex-col"
+                >
+                  <h2 className="sm:text-base text-sm pb-3 font-sans underline underline-offset-2 text-gray-800 group-hover:text-blue-600 transition-colors line-clamp-2">
+                    {language.name}
+                  </h2>
+                  <p className="sm:text-base text-sm text-gray-700">
+                    {language.description}
+                  </p>
+                </a>
+              </article>
+            </li>
+          ))}
+        </ul>
+      </section>
 
-      <section className="space-y-6">
-        <h2 className="text-2xl font-serif font-normal tracking-tight text-gray-900">
-          Languages & Technologies
+      <section className="space-y-4">
+        <h2 className="sm:text-3xl text-2xl font-serif font-normal tracking-tight text-black pb-2">
+          Github Languages & Projects
         </h2>
         <Suspense fallback={<LanguageFilterSkeleton />}>
           <LanguageFilter languages={languages} repos={repos} />
--- src/app/writing/page.tsx
+++ src/app/writing/page.tsx
@@ -12,15 +12,15 @@ export default async function WritingPage() {
 
   return (
     <>
-      <h2 className="text-2xl font-serif font-normal tracking-tight text-gray-900">
-        From time-to-time
-      </h2>
-      <header>
-        <p className="text-base text-gray-700 leading-relaxed max-w-2xl font-sans">
-          Thoughts and musings on software development, leadership, and
-          technology.
+      <section className="space-y-4">
+        <h2 className="sm:text-3xl text-2xl font-serif font-normal tracking-tight text-black pb-2">
+          From time-to-time,
+        </h2>
+        <p className="sm:text-base text-sm text-gray-700">
+          Appearntly I will write; however, this is most likely broken as I
+          decided to spend days on choosing the proper font... WHY!
         </p>
-      </header>
+      </section>
       <div className="grid gap-8 md:grid-cols-2">
         {posts.map((post) => (
           <WritingPostCard key={post.slug} post={post} />

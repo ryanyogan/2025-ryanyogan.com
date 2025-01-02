2e0b37dad24fa010b3c4183d26120211e5d9f33c
Ryan Yogan <ryanyogan@gmail.com>
2025-01-01 21:57:57 -0600

    that work?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
+    <html lang="en" className={`${GeistSans.className}`}>
       <body className="font-sans bg-white antialiased text-gray-900">
         <div className="max-w-3xl lg:max-w-4xl xl:max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
           <main className="sm:mt-6 mt-4 mb-24">

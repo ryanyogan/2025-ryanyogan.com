e48e0a6c984a683447d8d2d6ff72aa6fb4d1b668
Yordis Prieto <yordis.prieto@gmail.com>
2025-01-06 18:42:13 -0600

    about page dark mode

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,84 +7,81 @@ export const metadata = {
 export default function AboutPage() {
   return (
     <>
-      <section className="space-y-4">
-        <h2 className="sm:text-3xl text-2xl font-serif font-normal tracking-tight text-black pb-2">
-          Hello I&apos;m Ryan,
-        </h2>
-        <p className="sm:text-base text-sm text-gray-700">
-          As an Engineering Leader and Software Developer. I&apos;m passionate
-          about crafting exceptional user experiences, developing rock-solid
-          distributed services, and nurturing a dynamic 🤝 team development.
-        </p>
-        <p className="sm:text-base text-sm text-gray-700">
-          My leadership style is rooted in empathy, curiosity, and cheerleading.
-          I believe in the power of a growth mindset, and I strive to create an
-          environemnt where everyone may learn, thrive, and have 🎉 fun.
-        </p>
-        <p className="sm:text-base text-sm text-gray-700">
-          My engineering approach is thatof a Technical Generalist, recognizing
-          the unique value each technology brings to the table. My mission is to
-          build and teach, leveraging my broad skill set to innovate and 💡
-          inspire.
-        </p>
+      <div className="space-y-20">
+        <section className="space-y-4">
+          <h2 className="text-3xl font-serif text-gray-800 dark:text-zinc-50">
+            Hello I&apos;m Ryan,
+          </h2>
+          <p className="font-sans text-zinc-800 dark:text-zinc-400 tracking-tight">
+            As an Engineering Leader and Software Developer. I&apos;m passionate
+            about crafting exceptional user experiences, developing rock-solid
+            distributed services, and nurturing a dynamic team development.
+          </p>
+          <p className="font-sans text-zinc-800 dark:text-zinc-400 tracking-tight">
+            My leadership style is rooted in empathy, curiosity, and
+            cheerleading. I believe in the power of a growth mindset, and I
+            strive to create an environemnt where everyone may learn, thrive,
+            and have fun.
+          </p>
+          <p className="font-sans text-zinc-800 dark:text-zinc-400 tracking-tight">
+            My engineering approach is thatof a Technical Generalist,
+            recognizing the unique value each technology brings to the table. My
+            mission is to build and teach, leveraging my broad skill set to
+            innovate and 💡 inspire.
+          </p>
+        </section>
+        <section className="space-y-4">
+          <h2 className="text-3xl font-serif text-gray-800 dark:text-zinc-50">
+            Okay, here&apos;s what LLM&apos;s have to say about me...
+          </h2>
+          <p className="font-sans text-zinc-800 dark:text-zinc-400 tracking-tight">
+            Embarking on my tech journey with a 386 computer, I quickly became
+            the family IT department, graduating from building PCs to conjuring
+            AOL &quot;proggies&quot; in Visual Basic. My love affair with server
+            racks in chilly rooms was just the beginning, and my entry into
+            theater! Fast forward through a symphony of C#, PHP spells, and
+            Rails-powered rollercoasters, and you&apos;ll find me professing my
+            love for all things engineering.
+          </p>
+          <p className="font-sans text-zinc-800 dark:text-zinc-400 tracking-tight">
+            These days, I&apos;m crafting video game masterpieces in Rust—so
+            &quot;unique&quot; they&apos;re practically art, and hoarding
+            leadership books like they&apos;re going out of style. My toolkit? A
+            kaleidoscope of TypeScript frameworks so infamous, they dare not
+            speak their names.
+          </p>
+          <p className="font-sans text-zinc-800 dark:text-zinc-400 tracking-tight">
+            I&apos;ve navigated the startup scene from a cozy nook above a
+            Chicago bar to the dizzying heights of corporate skyscrapers, and I
+            love them all. Amidst a quirky relationship with Emacs and Clojure
+            (we&apos;re complicated), I find Zen in configuring Neovim more than
+            I do in housekeeping. I occasionally pine for the days of wrestling
+            with ColdFusion, Perl, and coaxing that last table and gif into
+            pixel-perfect harmony for IE6—ah, simpler times.
+          </p>
+          <p className="font-sans text-zinc-800 dark:text-zinc-400 tracking-tight">
+            My home doubles as an aircraft hangar, thanks to an obsession with
+            tinkering on aircrafts and simulators. I&apos;ve taken &quot;home
+            office&quot; to new altitudes, recreating a commercial flight
+            experience that&apos;s just one cranky passenger short of actual
+            takeoff.
+          </p>
+        </section>
 
-        <p className="sm:text-base text-sm text-gray-700">
-          I made a new site, with my own design for once lmao. I put some
-          serious thought into the spacing, fonts, and simplicity. I want to
-          write more, I cannot stand my voice on YouTube. I may ramble about
-          leadership, engineering, life, aviation... why not, my parents are a
-          majority of my traffic, hi mom 👋.
-        </p>
-
-        <h2 className="sm:text-3xl text-2xl font-serif font-normal tracking-tight text-black pb-2 pt-12">
-          Okay, here&apos;s what LLM&apos;s have to say about me...
-        </h2>
-
-        <p className="sm:text-base text-sm text-gray-700">
-          Embarking on my tech journey with a 386 computer, I quickly became the
-          family IT department, graduating from building PCs to conjuring AOL
-          &quot;proggies&quot; in Visual Basic. My love affair with server racks
-          in chilly rooms was just the beginning, and my entry into theater!
-          Fast forward through a symphony of C#, PHP spells, and Rails-powered
-          rollercoasters, and you&apos;ll find me professing my love for all
-          things engineering.
-        </p>
-        <p className="sm:text-base text-sm text-gray-700">
-          These days, I&apos;m crafting video game masterpieces in Rust—so
-          &quot;unique&quot; they&apos;re practically art, and hoarding
-          leadership books like they&apos;re going out of style. My toolkit? A
-          kaleidoscope of TypeScript frameworks so infamous, they dare not speak
-          their names.
-        </p>
-
-        <p className="sm:text-base text-sm text-gray-700">
-          I&apos;ve navigated the startup scene from a cozy nook above a Chicago
-          bar to the dizzying heights of corporate skyscrapers, and I love them
-          all. Amidst a quirky relationship with Emacs and Clojure (we&apos;re
-          complicated), I find Zen in configuring Neovim more than I do in
-          housekeeping. I occasionally pine for the days of wrestling with
-          ColdFusion, Perl, and coaxing that last table and gif into
-          pixel-perfect harmony for IE6—ah, simpler times.
-        </p>
-        <p className="sm:text-base text-sm text-gray-700">
-          My home doubles as an aircraft hangar, thanks to an obsession with
-          tinkering on aircrafts and simulators. I&apos;ve taken &quot;home
-          office&quot; to new altitudes, recreating a commercial flight
-          experience that&apos;s just one cranky passenger short of actual
-          takeoff.
-        </p>
-        <h2 className="sm:text-3xl text-2xl font-serif font-normal tracking-tight text-black pb-2 pt-12">
-          What matters most to me?
-        </h2>
-        <ul>
-          <li>👨‍👩‍👦‍👦 is my love.</li>
-          <li>🛩 is my joy.</li>
-          <li>🎸 is my jam.</li>
-          <li>🐕 & 🐈‍⬛ pain in the ass.</li>
-          <li>🚙 detailing cars is my zen.</li>
-          <li>🏒 full-time ice dad.</li>
-        </ul>
-      </section>
+        <section className="space-y-4">
+          <h2 className="text-3xl font-serif text-gray-800 dark:text-zinc-50">
+            What matters most to me?
+          </h2>
+          <ul className="text-gray-800 dark:text-zinc-50">
+            <li>👨‍👩‍👦‍👦 is my love.</li>
+            <li>🛩 is my joy.</li>
+            <li>🎸 is my jam.</li>
+            <li>🐕 & 🐈‍⬛ pain in the ass.</li>
+            <li>🚙 detailing cars is my zen.</li>
+            <li>🏒 full-time ice dad.</li>
+          </ul>
+        </section>
+      </div>
     </>
   );
 }
